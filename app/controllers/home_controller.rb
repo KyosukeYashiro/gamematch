bb9f57068b439566d16cc83b792eb505a09458cf
KyosukeYashiro <sola.smash.0846@gmail.com>
2021-01-15 16:55:10 +0900

    スタイリング修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
     def top
         @users=User.select("name","id","value","image","fee")
+        @games=Game.select("name","image")
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_133557) do
+ActiveRecord::Schema.define(version: 2021_01_15_061714) do
 
   create_table "game_users", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_01_08_133557) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
